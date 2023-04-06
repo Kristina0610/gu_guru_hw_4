6118911ccae7c41bba8c303163a1be974e455202
Kristina0610 <gaevskaya-2011@mail.ru>
2023-04-06 14:41:21 +0300

    working with data types

---
+++ src/main/java/hw_4/hw_4_1.java
@@ -0,0 +1,24 @@
+package hw_4;
+
+//поупражняться с математическими и логическими операторами,
+// добиться переполнения при вычислениях,
+// посмотреть результаты (вывести в консоль)
+
+public class hw_4_1 {
+  public static void main(String[] args) {
+    byte varByte = 10 + 127;
+    short varShort = 10 - 32787;
+    int varInt =1 * 2147483648;
+
+    System.out.println(varInt);
+    System.out.println(varByte);
+    System.out.println(varShort);
+
+    int age = 6;
+    boolean canGoToSchool = age >= 6 && age < 18;
+    System.out.println(canGoToSchool);
+
+
+  }
+
+}
---
+++ src/main/java/hw_4/hw_4_2.java
@@ -0,0 +1,13 @@
+package hw_4;
+
+// 2) попробовать вычисления комбинаций типов данных (int и double)
+public class hw_4_2 {
+  public static void main(String[] args) {
+    int varIntTest = 10;
+    double varDoubleTest = 1.0;
+    double result = varIntTest + varDoubleTest;
+    int result2 = varIntTest + (int)varDoubleTest;
+    System.out.println(result);
+    System.out.println(result2);
+  }
+}
